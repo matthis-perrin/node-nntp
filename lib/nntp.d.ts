2346a9fb49e3f7d0c8f991817f30ec1c4cfca856
Matthis Perrin <matthis.perrin@gmail.com>
2024-01-12 16:42:02 +0100

    Add quit method

---
+++ lib/nntp.d.ts
@@ -0,0 +1,36 @@
+declare module "nntp" {
+  export class NNTP {
+    public constructor();
+    public on(ev: "ready", cb: () => void): void;
+    public on(ev: "error", cb: (err: Error) => void): void;
+    public on(ev: "close", cb: (hasErr: boolean) => void): void;
+    public group(
+      grp: string,
+      cb: (
+        err: Error | undefined,
+        count: number,
+        first: number,
+        last: number
+      ) => void
+    ): void;
+    public headers(
+      name: number | string,
+      cb: (
+        err: Error | undefined,
+        num: number,
+        id: string,
+        headers: Record<string, unknown>
+      ) => void
+    ): void;
+    public end(): void;
+    public connect(opts: {
+      host: string;
+      port: number;
+      secure: true;
+      user: string;
+      password: string;
+    }): void;
+    public quit(cb: (err: Error | undefined) => void): void;
+  }
+  export = NNTP;
+}
